795081cf3def533cd2babd745f63abbf81c96907
Taras Shapovalov <tarasshapovalov85@gmail.com>
2022-10-02 17:30:27 +0300

    add all logic and lib

---
+++ README.md
@@ -0,0 +1,4 @@
+"list" --- https://monosnap.com/file/EzJOlchBWhniphL8R4yaVZDrmUtDfx
+"byId" --- https://monosnap.com/file/WPmksh9luIJx7ulvswkIlwtCY4yNlY
+"add" --- https://monosnap.com/file/UK5HcBdLZk7TlbX6NbmWHEERRKpCHS
+"remove" --- https://monosnap.com/file/YoE4tkibIch9c8ZoMZHAl5xV7SDDvl
--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-console.log("Hello Taras it is Node")
+const contacts = require("./contacts");
+const argv = require("yargs").argv;
+
+const invokeAction = async ({ action, id, name, email, phone }) => {
+  switch (action) {
+    case "list":
+      const listContacts = await contacts.listContacts();
+      console.table(listContacts);
+      break;
+    case "get":
+      const oneContact = await contacts.getContactById(id);
+      console.log(oneContact);
+      break;
+    case "remove":
+      const removeContact = await contacts.removeContact(id);
+      console.log(removeContact);
+      break;
+    case "add":
+      const newContact = await contacts.addContact({ name, email, phone });
+      console.log(newContact);
+      break;
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+};
+
+invokeAction(argv);
